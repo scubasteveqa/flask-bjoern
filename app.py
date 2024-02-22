b4ffa556b9e20900c96cccae9e86a2e64c234ae6
scubasteveqa <154364741+scubasteveqa@users.noreply.github.com>
2024-02-22 10:13:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World! This app was made without certain packages in the requirements.txt" 
 
 if __name__ == "__main__":
     import bjoern
